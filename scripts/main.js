0026c0cc4e615d27f838f0c06da44e487ea2eeee
Ipshita Chatterjee <ipshita1996@gmail.com>
2018-06-25 21:23:28 +0530

    can now color in black

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded',function(e){
     const grid=document.querySelector('#grid');
     
-    for(var j=0;j<16;j++){
+    for(var j=0;j<20;j++){
         const row=document.createElement('div');
         row.classList.add('rows');
-        for(var i=0;i<16;i++){
+        for(var i=0;i<20;i++){
             const cell=document.createElement('div');
             cell.classList.add('in-grid');
             row.appendChild(cell);
+            cell.addEventListener('mouseover',function(e){
+                e.target.style.background='black';
+            });
         }
         grid.appendChild(row);
     }
-    
 });
