804628746dfd5f08a964a16eb9c4acd34a2ca2b2
Ipshita Chatterjee <ipshita1996@gmail.com>
2018-06-26 10:40:49 +0530

    working functionality of grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,44 @@
 document.addEventListener('DOMContentLoaded',function(e){
+    const btn=document.querySelector('#reset');
     const grid=document.querySelector('#grid');
-    
-    for(var j=0;j<20;j++){
-        const row=document.createElement('div');
-        row.classList.add('rows');
-        for(var i=0;i<20;i++){
-            const cell=document.createElement('div');
-            cell.classList.add('in-grid');
-            row.appendChild(cell);
-            cell.addEventListener('mouseover',function(e){
-                e.target.style.background='black';
-            });
+
+    creategrid(16);
+    btn.addEventListener('click',function(e){
+        //reset function
+        n=prompt('how many squares per side to make the new grid?');
+        if(n>35){
+            alert('enter a number less than 35');
         }
-        grid.appendChild(row);
-    }
+        else{
+            while(grid.firstChild){
+                grid.removeChild(grid.firstChild);
+            }
+            creategrid(n);
+        }
+        
+    });
+    
+    function creategrid(x){
+        console.log('creating');
+        for(var j=0;j<x;j++){
+            console.log(x);
+            const row=document.createElement('div');
+            row.classList.add('rows');
+            for(var i=0;i<x;i++){
+                const cell=document.createElement('div');
+                cell.classList.add('in-grid');
+                var size=450/x;
+                cell.style.width=size+'px';
+                cell.style.height=size+'px';
+                row.appendChild(cell);
+                cell.addEventListener('mouseover',function(e){
+                    e.target.style.background='black';
+                });
+            }
+            grid.appendChild(row);
+        }
+    };
+    
+    
 });
+
